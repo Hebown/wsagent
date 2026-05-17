b3456a78f0301984129635ffdd5232b259886cbd
Hebown <151939174+Hebown@users.noreply.github.com>
2026-05-17 17:40:49 +0800

    优化了用户端信息显示

--- src/extension.ts
+++ src/extension.ts
@@ -11,16 +11,21 @@ const workspaceDirPath=getWorkspaceRoot().fsPath;
 dotenv.config({ path: path.join(workspaceDirPath, '.env') });
 
 export function activate(context: vscode.ExtensionContext) {
-    // 1. 初始化 LLM 客户端（建议从环境变量或配置中读取 API Key）
+    // 1. 初始化 LLM 客户端
 	vscode.window.showInformationMessage('WSAgent 已启动！');
-	vscode.window.showInformationMessage(path.join(workspaceDirPath,'.env'));
+    
+	vscode.window.showInformationMessage("尝试在 "+path.join(workspaceDirPath,'.env')+" 下加载 env");
     const apiKey=process.env.DEEPSEEK_API_KEY as string;
+    if(!apiKey){
+        vscode.window.showErrorMessage("未能成功加载 env 中的apiKey，请检查后重试");
+        return;
+    }
     const llmClient = new DeepSeekClient(apiKey);
 
     // 2. 初始化工具库
     const toolRegistry = createDefaultToolRegistry();
 
-    // 3. 注册聊天参与者（前端交互核心）
+    // 3. 注册聊天参与者
     registerChatParticipant(context, llmClient, toolRegistry);
 
     console.log('WSAgent 已经激活！');
